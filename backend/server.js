67da0b5c33cae7573498b050c9f8ceec7a4aae01
Samir Khoury <samir@khoury.tk>
2022-12-17 16:42:02 +0200

    connected the database

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,25 @@
+require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
-const app = express();
 //Routes
 const managersRoutes = require("./routes/managers");
 const tenantsRoutes = require("./routes/tenants");
 
+//express app
+const app = express();
 const port = 4000;
+
+//middleware
+//CORS
+const corsOptions = {
+  origin: ["http://localhost:3000",process.env.LCL_IP], //for the meantime only accept from localhost
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
+//JSON Parser
+app.use(express.json());
 
 //debug logger
 app.use((req, res, next) => {
@@ -13,10 +27,21 @@ app.use((req, res, next) => {
   next();
 });
 
+//routes
 app.use("/api/managers", managersRoutes);
 app.use("/api/tenants", tenantsRoutes);
 
-// Run the server
-app.listen(port, () => {
-  console.log(`Server running on port: ${port}`);
+// Make the Queries Strict and Remove Deprecation Warning
+mongoose.set("strictQuery", true);
+// Connect to DB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("connected to database");
+    // Run the server on the port
+    app.listen(port, () => {
+      console.log(`Server running on port: ${port}`);
+    });
+})
+.catch((err)=>{
+  console.log(err);
 });
