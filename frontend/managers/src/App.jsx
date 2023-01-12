57a3f20c096e7a414780486846a934e0b679b944
Samir Khoury <samir@khoury.tk>
2023-01-12 17:31:46 +0200

    sidebar basic import

--- frontend/managers/src/App.jsx
+++ frontend/managers/src/App.jsx
@@ -2,11 +2,13 @@ import React from "react";
 
 //Pages and Components
 import Navbar from "../src/components/Navbar";
+import Sidebar from "../src/components/Sidebar";
 
 function App() {
   return (
   <div className="App">
     <Navbar/>
+    <Sidebar/>
   </div>
   );
 }
