4368c5e5025629349dde897139aac9e004cac63b
Samir Khoury <samir@khoury.tk>
2023-01-14 21:30:22 +0200

    removed unused elements

--- frontend/managers/src/components/modals/DeleteConfirmation.jsx
+++ frontend/managers/src/components/modals/DeleteConfirmation.jsx
@@ -4,7 +4,6 @@ import { useModalsContext } from "../../hooks/useModalsContext";
 
 //bootstrap components
 import Modal from "react-bootstrap/Modal";
-import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 
 function DeleteConfirmation({ deleteData }) {
--- frontend/managers/src/hooks/useModalsContext.js
+++ frontend/managers/src/hooks/useModalsContext.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import { useContext } from "react";
 import { ModalsContext } from "../context/ModalsContext";
 
 export function useModalsContext() {
