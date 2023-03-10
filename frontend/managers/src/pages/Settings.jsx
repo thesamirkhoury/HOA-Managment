9ca55d044d2d185872517df70bc323abd06dee7b
Samir Khoury <samir@khoury.tk>
2023-03-10 23:11:51 +0200

    Settings Page

--- frontend/managers/src/pages/Settings.jsx
+++ frontend/managers/src/pages/Settings.jsx
@@ -1,10 +1,70 @@
 import React from "react";
 
+//bootstrap components
+import Button from "react-bootstrap/Button";
+//bootstrap spacing
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
 function Settings() {
   return (
-    <div>
-      <h1>Settings</h1>
-    </div>
+    <>
+      {/* Page Name */}
+      <h1 className="display-1">הגדרות</h1>
+
+      {/* HOA Details */}
+      <h1 className="display-3">פרטי הועד והבניין</h1>
+
+      <div>
+        <p>
+          כתובת הבניין:
+          <span>{"יעקוב שרייבום 26, רמת בית הכרם, ירושלים, ישראל."}</span>
+        </p>
+
+        <p>
+          שם ראש הועד:
+          <span>{"ישראל ישראלי"}</span>
+        </p>
+
+        <p>
+          מספר הבניינים:
+          <span>{"3"}</span>
+        </p>
+
+        <p>
+          דמי הועד החושדיים:
+          <span>{"250₪"}</span>
+        </p>
+      </div>
+      <Row xs={7} md={6}>
+        <Button variant="outline-info" className="ms-2">
+          עדכן פרטים
+        </Button>
+      </Row>
+
+      {/* Personal Details */}
+      <h1 className="display-3">פרטים אישיים</h1>
+
+      <div>
+        <p>
+          מייל:
+          <span>{"demo@demo.com"}</span>
+        </p>
+
+        <p>
+          מספר טלפון:
+          <span>{"0521234567"}</span>
+        </p>
+      </div>
+      <Row xs={7} md={6}>
+        <Button variant="info" className="ms-2 mb-2 mb-md-0">
+          עדכון פרטיים אישיים
+        </Button>
+        <Button variant="outline-info" className="ms-2">
+          עדכון סיסמה
+        </Button>
+      </Row>
+    </>
   );
 }
 
