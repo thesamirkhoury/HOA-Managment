562780716300d9fb8fb5d395473dfaae8a0b5712
Samir Khoury <samir@khoury.tk>
2023-03-11 20:59:33 +0200

    basic pages and components

---
+++ frontend/tenants/src/pages/Financial.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Financial() {
+  return (
+    <div>
+      <h1>Financial</h1>
+    </div>
+  );
+}
+
+export default Financial;
---
+++ frontend/tenants/src/pages/Maintenance.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Maintenance() {
+  return (
+    <div>
+      <h1>Maintenance</h1>
+    </div>
+  );
+}
+
+export default Maintenance;
