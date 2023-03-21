9eb314499f86d5e86afefd708838f017cd9bbfb3
Samir Khoury <samir@khoury.tk>
2023-03-21 19:25:32 +0200

    404 Page

--- frontend/tenants/src/pages/NotFound.jsx
+++ frontend/tenants/src/pages/NotFound.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 
-function NotFound() {
+//bootstrap components
+import Button from "react-bootstrap/Button";
+//import Router Nav Link
+import { LinkContainer } from "react-router-bootstrap";
+
+function Notfound() {
   return (
-    <div>
-      <h1>NotFound</h1>
+    <div className="text-center text-dark">
+      <h1 className="fs-huge">404</h1>
+      <h2 className="fs-1">לא מצאנו את העמוד שחיפסת.</h2>
+      <LinkContainer to="/">
+        <Button variant="dark" onClick={() => {}}>
+          לעמוד הבית
+        </Button>
+      </LinkContainer>
     </div>
   );
 }
 
-export default NotFound;
+export default Notfound;
