4d8f4929a0b9d59921243950c9434bd3ade505cb
Samir Khoury <samir@khoury.tk>
2023-03-21 19:23:50 +0200

    Loader Modal

---
+++ frontend/tenants/src/components/Loader.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import { useModalsContext } from "../hook/useModalsContext";
+
+//bootstrap components
+import Modal from "react-bootstrap/Modal";
+import Spinner from "react-bootstrap/Spinner";
+
+function Loader() {
+  const { loading } = useModalsContext();
+
+  return (
+    <Modal
+      show={loading}
+      className="text-center loader-spinner"
+      backdrop="static"
+      keyboard={false}
+      centered={true}
+    >
+      <Modal.Body>
+        <Spinner animation="grow" size="xl" />
+      </Modal.Body>
+    </Modal>
+  );
+}
+
+export default Loader;
