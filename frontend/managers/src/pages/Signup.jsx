b3e588af2488f8505b6c8585eb899c477c07cff1
Samir Khoury <samir@khoury.tk>
2023-03-22 17:27:43 +0200

    signup form connect

--- frontend/managers/src/pages/Signup.jsx
+++ frontend/managers/src/pages/Signup.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 
 //bootstrap components
-import Form from "react-bootstrap/Form";
 import Card from "react-bootstrap/Card";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 //bootstrap spacing
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
@@ -16,23 +17,43 @@ function Signup() {
   const [fee, setFee] = useState("");
   const [buildingCount, setBuildingCount] = useState("");
 
+  async function handleSignup(e) {
+    e.preventDefault();
+
+  }
+
   return (
     <div className="mt-1 mt-md-5 ms-2">
-      <Card className="">
+      <Card>
         <Card.Body>
           <h1 className="display-1 text-center">הרשמת ועד בית חדש</h1>
-          <Form className="m-2">
+
+          <Form className="m-2" onSubmit={handleSignup}>
             <Row>
               <Col md={6} className="mb-2">
                 <Form.Group>
                   <Form.Label>שם פרטי</Form.Label>
-                  <Form.Control type="text" required></Form.Control>
+                  <Form.Control
+                    type="text"
+                    required
+                    value={firstName}
+                    onChange={(e) => {
+                      setFirstName(e.target.value);
+                    }}
+                  ></Form.Control>
                 </Form.Group>
               </Col>
               <Col className="mb-2">
                 <Form.Group>
                   <Form.Label>שם משפחה</Form.Label>
-                  <Form.Control type="text" required></Form.Control>
+                  <Form.Control
+                    type="text"
+                    required
+                    value={lastName}
+                    onChange={(e) => {
+                      setLastName(e.target.value);
+                    }}
+                  ></Form.Control>
                 </Form.Group>
               </Col>
             </Row>
@@ -41,7 +62,14 @@ function Signup() {
               <Col className="mb-2">
                 <Form.Group>
                   <Form.Label>מייל</Form.Label>
-                  <Form.Control type="email" required></Form.Control>
+                  <Form.Control
+                    type="email"
+                    required
+                    value={email}
+                    onChange={(e) => {
+                      setEmail(e.target.value);
+                    }}
+                  ></Form.Control>
                 </Form.Group>
               </Col>
             </Row>
@@ -50,7 +78,14 @@ function Signup() {
               <Col md={6} className="mb-2">
                 <Form.Group>
                   <Form.Label>סיסמה</Form.Label>
-                  <Form.Control type="password" required></Form.Control>
+                  <Form.Control
+                    type="password"
+                    required
+                    value={password}
+                    onChange={(e) => {
+                      setPassword(e.target.value);
+                    }}
+                  ></Form.Control>
                 </Form.Group>
               </Col>
               <Col className="mb-2">
@@ -70,7 +105,14 @@ function Signup() {
               <Col className="mb-2">
                 <Form.Group>
                   <Form.Label>כתובת מלאה</Form.Label>
-                  <Form.Control type="text" required></Form.Control>
+                  <Form.Control
+                    type="text"
+                    required
+                    value={address}
+                    onChange={(e) => {
+                      setAddress(e.target.value);
+                    }}
+                  ></Form.Control>
                 </Form.Group>
               </Col>
             </Row>
@@ -84,6 +126,10 @@ function Signup() {
                     inputMode="numeric"
                     min="1"
                     required
+                    value={buildingCount}
+                    onChange={(e) => {
+                      setBuildingCount(e.target.value);
+                    }}
                   ></Form.Control>
                 </Form.Group>
               </Col>
@@ -96,8 +142,18 @@ function Signup() {
                     min="1"
                     step="any"
                     required
+                    value={fee}
+                    onChange={(e) => {
+                      setFee(e.target.value);
+                    }}
                   ></Form.Control>
                 </Form.Group>
+              </Col>
+            </Row>
+
+            <Row>
+              <Col md={{ span: 5, offset: 3 }} className="">
+                <Button className="w-100">הירשם</Button>
               </Col>
             </Row>
           </Form>
