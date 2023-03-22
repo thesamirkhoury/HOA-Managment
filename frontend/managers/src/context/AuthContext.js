02363de4822e4265449e10f2ca9c8cb1042dab62
Samir Khoury <samir@khoury.tk>
2023-03-22 20:45:49 +0200

    check if user logged in

--- frontend/managers/src/context/AuthContext.js
+++ frontend/managers/src/context/AuthContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useReducer } from "react";
+import React, { createContext, useReducer, useEffect } from "react";
 
 export const AuthContext = createContext();
 
@@ -18,8 +18,13 @@ export const AuthContextProvider = ({ children }) => {
     user: null,
   });
 
-  console.log("AuthContext state:", state);
-  
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("board"));
+    if (user) {
+      dispatch({ type: "LOGIN", payload: user });
+    }
+  }, []);
+
   return (
     <AuthContext.Provider value={{ ...state, dispatch }}>
       {children}
