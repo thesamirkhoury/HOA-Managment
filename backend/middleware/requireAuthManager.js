dfaea13f279063bff476dc418a7cba58bf74d8f8
Samir Khoury <samir@khoury.tk>
2023-03-22 22:05:06 +0200

    manager auth middleware

---
+++ backend/middleware/requireAuthManager.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken");
+const HOA = require("../models/hoa");
+
+async function requireAuthManager(req, res, next) {
+  //verify auth
+  const { authorization } = req.headers;
+  if (!authorization) {
+    return res.status(401).json("Auth token required");
+  }
+
+  const token = authorization.split(" ")[1];
+
+  //verify token.
+  try {
+    const { _id } = jwt.verify(token, process.env.SECRET);
+    req.user = await HOA.findOne({ _id }).select("_id");
+    next();
+  } catch (error) {
+    res.status(401).json({ error: "Request is not authorized" });
+  }
+}
+
+module.exports = requireAuthManager;
