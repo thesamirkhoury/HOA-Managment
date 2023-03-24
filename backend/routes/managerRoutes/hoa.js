b96453b57c4c9b255991eb0634298295389375ce
Samir Khoury <samir@khoury.tk>
2023-03-24 11:30:24 +0300

    moved auth functions to stand-alone route

--- backend/routes/managerRoutes/hoa.js
+++ backend/routes/managerRoutes/hoa.js
@@ -2,20 +2,12 @@ const express = require("express");
 
 // controller functions
 const {
-  signup,
-  login,
   getAllDetails,
   editHoa,
   deleteHoa,
 } = require("../../controllers/hoaControllers");
 
 const router = express.Router();
-
-//Create a hoa (signup)
-router.post("/signup", signup);
-
-//login as hoa manager
-router.post("/login", login);
 
 //Get the hoa info
 router.get("/", getAllDetails);
