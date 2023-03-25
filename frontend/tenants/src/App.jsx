8214383771755268c28be16ab7361f45ecf97fc2
Samir Khoury <samir@khoury.tk>
2023-03-25 19:09:53 +0300

    protecting react route

--- frontend/tenants/src/App.jsx
+++ frontend/tenants/src/App.jsx
@@ -34,19 +34,65 @@ function App() {
         {/* Content */}
         <div className="content ps-2">
           <Routes>
-            {/* Main Page - Redirect to the first page in the dashboard */}
-            <Route path="/" element={<Navigate to="announcements" />} exact />
+            {/* Main Page - Redirect to the first page in the dashboard if user is logged in, else redirect to login page */}
+            <Route
+              path="/"
+              element={
+                user ? (
+                  <Navigate to="/announcements" />
+                ) : (
+                  <Navigate to="/login" />
+                )
+              }
+              exact
+            />
             {/* Dashboard pages */}
-            <Route path="announcements" element={<Announcements />} exact />
-            <Route path="maintenance" element={<Maintenance />} exact />
-            <Route path="inquires" element={<Inquires />} exact />
-            <Route path="billings" element={<Billing />} exact />
-            <Route path="financial" element={<Financial />} exact />
-            <Route path="documents" element={<Documents />} exact />
-            <Route path="hoa-details" element={<HOADetails />} exact />
-            <Route path="settings" element={<Settings />} exact />
+            <Route
+              path="announcements"
+              element={user ? <Announcements /> : <Navigate to="/login" />}
+              exact
+            />
+            <Route
+              path="maintenance"
+              element={user ? <Maintenance /> : <Navigate to="/login" />}
+              exact
+            />
+            <Route
+              path="inquires"
+              element={user ? <Inquires /> : <Navigate to="/login" />}
+              exact
+            />
+            <Route
+              path="billings"
+              element={user ? <Billing /> : <Navigate to="/login" />}
+              exact
+            />
+            <Route
+              path="financial"
+              element={user ? <Financial /> : <Navigate to="/login" />}
+              exact
+            />
+            <Route
+              path="documents"
+              element={user ? <Documents /> : <Navigate to="/login" />}
+              exact
+            />
+            <Route
+              path="hoa-details"
+              element={user ? <HOADetails /> : <Navigate to="/login" />}
+              exact
+            />
+            <Route
+              path="settings"
+              element={user ? <Settings /> : <Navigate to="/login" />}
+              exact
+            />
             {/* Auth pages */}
-            <Route path="login" element={<Login />} exact />
+            <Route
+              path="login"
+              element={!user ? <Login /> : <Navigate to="/" />}
+              exact
+            />
             {/* 404 Page for non-existing pages */}
             <Route path="*" element={<NotFound />} exact />
           </Routes>
