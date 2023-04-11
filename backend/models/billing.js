f43c01a26bca60548aba9dad656d6b186b3a91b3
Samir Khoury <samir@khoury.tk>
2023-04-11 21:05:29 +0300

    moving sum to a statics function

--- backend/models/billing.js
+++ backend/models/billing.js
@@ -40,4 +40,67 @@ const billingSchema = new Schema(
   { timestamps: true }
 );
 
+// static sum given a period of time function
+//TODO: Change Error messages to hebrew
+billingSchema.statics.sum = async function (hoa_id, startDate, endDate) {
+  if (!hoa_id || !startDate || !endDate) {
+    throw Error("All fields must be filled");
+  }
+  // search for all the bills,created by th HOA ID grouped by updated at month, (with the status of paid)
+  const existingIncomes = await this.aggregate([
+    {
+      $match: {
+        // find paid documents from start date to end date, created by th HOA ID
+        hoa_id: hoa_id.toString(),
+        updatedAt: {
+          $gte: startDate,
+          $lte: endDate,
+        },
+        paymentStatus: "שולם",
+      },
+    },
+    {
+      $group: {
+        // sum the amount
+        _id: { month: { $month: "$updatedAt" }, year: { $year: "$updatedAt" } },
+        sum: { $sum: "$amount" },
+      },
+    },
+    {
+      $sort: {
+        "_id.year": 1,
+        "_id.month": 1,
+      },
+    },
+  ]);
+
+  let incomes = [];
+  let currMonth = startDate;
+  //iterate over the provided time period, if the month is available append it to the incomes array, if it is not available append a sum of zero
+  while (currMonth <= endDate) {
+    //search for the month in the aggregated results from the DB
+    const existingIncome = existingIncomes.find(
+      (income) => income._id.month === currMonth.getMonth() + 1
+    );
+
+    // check if the month is NOT available in the DB, to append a sum of zero for that month.
+    if (!existingIncome) {
+      incomes.push({
+        date: `${currMonth.getFullYear()}-${currMonth.getMonth() + 1}`,
+        sum: 0,
+      });
+    }
+    // if the month is available in the DB, append the sum from the aggregation array.
+    else {
+      incomes.push({
+        date: `${existingIncome._id.year}-${existingIncome._id.month}`,
+        sum: existingIncome.sum,
+      });
+    }
+
+    // increase the months by one
+    currMonth.setMonth(currMonth.getMonth() + 1);
+  }
+  return incomes;
+};
 module.exports = mongoose.model("Billing", billingSchema);
--- backend/models/expense.js
+++ backend/models/expense.js
@@ -36,4 +36,69 @@ const expenseSchema = new Schema(
   { timestamps: true }
 );
 
+// static sum given a period of time function
+//TODO: Change Error messages to hebrew
+expenseSchema.statics.sum = async function (hoa_id, startDate, endDate) {
+  if (!hoa_id || !startDate || !endDate) {
+    throw Error("All fields must be filled");
+  }
+  // search for all the expenses,created by th HOA ID grouped by created month
+  const existingExpenses = await this.aggregate([
+    {
+      $match: {
+        // find paid documents from start date to end date, created by th HOA ID
+        hoa_id: hoa_id.toString(),
+        createdAt: {
+          $gte: startDate,
+          $lte: endDate,
+        },
+      },
+    },
+    {
+      $group: {
+        // sum the amount
+        _id: {
+          month: { $month: "$paymentDate" },
+          year: { $year: "$paymentDate" },
+        },
+        sum: { $sum: "$amount" },
+      },
+    },
+    {
+      $sort: {
+        "_id.year": 1,
+        "_id.month": 1,
+      },
+    },
+  ]);
+
+  let expenses = [];
+  let currMonth = startDate;
+  //iterate over the provided time period, if the month is available append it to the expenses array, if it is not available append a sum of zero
+  while (currMonth <= endDate) {
+    //search for the month in the aggregated results from the DB
+    const existingExpense = existingExpenses.find(
+      (expense) => expense._id.month === currMonth.getMonth() + 1
+    );
+
+    // check if the month is NOT available in the DB, to append a sum of zero for that month.
+    if (!existingExpense) {
+      expenses.push({
+        date: `${currMonth.getFullYear()}-${currMonth.getMonth() + 1}`,
+        sum: 0,
+      });
+    }
+    // if the month is available in the DB, append the sum from the aggregation array.
+    else {
+      expenses.push({
+        date: `${existingExpense._id.year}-${existingExpense._id.month}`,
+        sum: existingExpense.sum,
+      });
+    }
+
+    // increase the months by one
+    currMonth.setMonth(currMonth.getMonth() + 1);
+  }
+  return expenses;
+};
 module.exports = mongoose.model("Expense", expenseSchema);
