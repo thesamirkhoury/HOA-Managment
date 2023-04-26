5c4e0f8d57d31616afc8e42777d3241b4a0a5c27
Samir Khoury <samir@khoury.tk>
2023-04-26 22:02:58 +0300

    selling points section

--- frontend/landing/src/App.jsx
+++ frontend/landing/src/App.jsx
@@ -4,12 +4,14 @@ import React from "react";
 import Navbar from "./components/Navbar";
 //Sections
 import Hero from "./components/Sections/Hero";
+import SellingPoints from "./components/Sections/SellingPoints";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero />
+      <SellingPoints/>
     </div>
   );
 }
