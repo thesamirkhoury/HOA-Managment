74eac1f2b7e6156be0dcb1708d383d1f8b8239b3
Samir Khoury <samir@khoury.tk>
2023-04-26 13:14:54 +0300

    installing bootstrap

--- frontend/landing/src/index.js
+++ frontend/landing/src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+// RTL Bootstrap
+import "bootstrap/dist/css/bootstrap.rtl.min.css";
+// styling
+import "./style.scss";
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
