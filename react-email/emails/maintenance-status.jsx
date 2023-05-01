1130baf6ec641da5efc2f6eb8288a08e6f3aaa45
Samir Khoury <samir@khoury.tk>
2023-05-01 23:32:04 +0300

    Maintenance status email style change

--- react-email/emails/maintenance-status.jsx
+++ react-email/emails/maintenance-status.jsx
@@ -15,8 +15,8 @@ import {
 
 function MaintenanceStatus() {
   //!placeholder data
-  let firstName = "ישראל";
-  let subject = "בדיקת קריאת שירות";
+  let firstName = "${tenant.firstName}";
+  let subject = "${requestSubject}";
 
   return (
     <Html lang="he" dir="rtl">
@@ -26,12 +26,13 @@ function MaintenanceStatus() {
         <Body className="bg-white my-auto mx-auto font-sans">
           <Container className="border border-solid border-[#eaeaea] rounded my-[40px] mx-auto p-[20px] w-[465px]">
             {/* Logo Section */}
-            <Section className="mt-[32px]">
+            <Section className="mt-[32px] grid justify-center">
               <Img
-                src={`/static/logo.svg`}
+                src={"${process.env.LOGO_URL}"}
                 width="212"
                 height="88"
                 alt="logo"
+                className="bg-white"
               />
             </Section>
             {/* Subject Section */}
@@ -39,10 +40,10 @@ function MaintenanceStatus() {
               <Heading className="text-black text-center font-normal text-[24px]">
                 עודכן סטטוס קריאת השירות שלך
               </Heading>
-              <Text className="text-black text-[18px] leading-[24px]">
+              <Text className="text-black text-center text-[18px] leading-[24px]">
                 שלום {firstName},
               </Text>
-              <Text className="text-black text-[18px] leading-[24px]">
+              <Text className="text-black text-center text-[18px] leading-[24px]">
                 התעדכן הסטטוס לקריאת השירות שפתחת בנושה {subject}.
                 <br />
                 לפרטים נוספים יש להכנס למערכת הדיירים.
@@ -55,7 +56,7 @@ function MaintenanceStatus() {
                 pX={20}
                 pY={12}
                 className="bg-[#000000] rounded text-white text-[12px] font-semibold no-underline text-center"
-                href={"http://localhost:3001/maintenance"}
+                href={"${process.env.TENANTS_URL}/maintenance"}
               >
                 כניסה למערכת
               </Button>
@@ -63,7 +64,7 @@ function MaintenanceStatus() {
 
             {/* Footer */}
             <Section>
-              <Text className="text-[#666666]">
+              <Text className="text-[#666666] text-center">
                 ההודעה הזאת נשלחה באופן אוטומאטי, כי המייל שלך מופיע במערכת
                 ניהול ועד בית כדייר.
               </Text>
