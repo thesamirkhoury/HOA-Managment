0685409a18c6c0d930c96cdebb854c0685430bc2
Samir Khoury <samir@khoury.tk>
2023-05-02 12:43:42 +0300

    reset password email update

--- react-email/emails/reset-password.jsx
+++ react-email/emails/reset-password.jsx
@@ -16,23 +16,24 @@ import {
 
 function ResetPassword() {
   //!placeholder data
-  let firstName = "ישראל";
-  let url = "123";
+  let firstName = "${firstName}";
+  let url = "${url}";
 
   return (
     <Html lang="he" dir="rtl">
       <Head />
-      <Preview>הושג עבורך דרישת תשלום חדשה</Preview>
+      <Preview>קישור לאיפוס הסיסמה</Preview>
       <Tailwind>
         <Body className="bg-white my-auto mx-auto font-sans">
           <Container className="border border-solid border-[#eaeaea] rounded my-[40px] mx-auto p-[20px] w-[465px]">
             {/* Logo Section */}
-            <Section className="mt-[32px]">
+            <Section className="mt-[32px] grid justify-center">
               <Img
-                src={`/static/logo.svg`}
+                src={"${process.env.LOGO_URL}"}
                 width="212"
                 height="88"
                 alt="logo"
+                className="bg-white"
               />
             </Section>
 
@@ -41,10 +42,10 @@ function ResetPassword() {
               <Heading className="text-black text-center font-normal text-[24px]">
                 שחכת את הסיסה שלך?
               </Heading>
-              <Text className="text-black text-[18px] leading-[24px]">
+              <Text className="text-black text-center text-[18px] leading-[24px]">
                 שלום {firstName},
               </Text>
-              <Text className="text-black text-[18px] leading-[24px]">
+              <Text className="text-black text-center text-[18px] leading-[24px]">
                 קיבלנו בקשה לאיפוס הסיסמה שלך, מצורף קישור לבחירת סיסמה חדשה.
               </Text>
             </Section>
@@ -59,14 +60,17 @@ function ResetPassword() {
               >
                 איפוס סיסמה
               </Button>
-              <Text className="text-black text-[14px] leading-[24px]">
+              <Text className="text-black text-center text-[14px] leading-[24px]">
                 או בקישור הבא:
                 <br />
-                <Link href={url} className="text-blue-600 no-underline">
+                <Link
+                  href={url}
+                  className="text-blue-600 no-underline text-center"
+                >
                   {url}
                 </Link>
               </Text>
-              <Text className="text-[12px]">
+              <Text className="text-[12px] text-center">
                 הקישור תקף ל-10 דקות בלבד.
                 <br />
                 אם לא ביקשת להחליף את הסיסמה ניתן להתעלם מהייל הזה.
@@ -75,7 +79,7 @@ function ResetPassword() {
 
             {/* Footer */}
             <Section>
-              <Text className="text-[#666666]">
+              <Text className="text-[#666666] text-center">
                 ההודעה הזאת נשלחה באופן אוטומאטי, כי המייל שלך מופיע במערכת
                 ניהול ועד בית כדייר.
               </Text>
