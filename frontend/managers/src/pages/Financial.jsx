9d9d46c616d5b8c0bf8e7f91742d5209a64db267
Samir Khoury <samir@khoury.tk>
2023-05-10 21:32:41 +0300

    download financial data to csv file

--- frontend/managers/src/pages/Financial.jsx
+++ frontend/managers/src/pages/Financial.jsx
@@ -7,6 +7,9 @@ import { useDataHandler } from "../hooks/useDataHandler";
 // ChartJS Bar chart Component
 import { Bar } from "react-chartjs-2";
 import { Chart as ChartJS } from "chart.js/auto"; // eslint-disable-line
+//CSV download file
+import { CSVLink } from "react-csv";
+
 //bootstrap components
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
@@ -24,11 +27,33 @@ function Financial() {
   const [labels, setLabels] = useState([]);
   const [incomeData, setIncomeData] = useState([]);
   const [spendingData, setSpendingData] = useState([]);
+  const [financialData, setFinancialData] = useState([]);
 
   async function getData(e) {
     e.preventDefault();
     await fetchData(`billing/sum/${fromDate}/${toDate}`, "SET_INCOME");
     await fetchData(`expenses/sum/${fromDate}/${toDate}`, "SET_SPENDING");
+  }
+
+  //combines the income and expenses alongside with the months labels, in rows format
+  async function combineDataData() {
+    if (labels && incomeData && spendingData) {
+      //create headers
+      let arr = [["חודש", "הכנסות", "הוצאות"]];
+      // make each month expense and income in on row
+      labels.forEach((label, index) => {
+        arr.push([label, incomeData[index], spendingData[index]]);
+      });
+      // add the creation date
+      let today = new Date(Date.now());
+      arr.push([
+        "----",
+        `הקובץ נוצר על ידי מערכת נהל - בתאריך: ${today}`,
+        "----",
+      ]);
+      // set the state
+      setFinancialData(arr);
+    }
   }
 
   useEffect(() => {
@@ -43,6 +68,7 @@ function Financial() {
       setSpendingData(sum);
     }
   }, [income, spending]);
+
   return (
     <>
       {/* Document Title */}
@@ -82,9 +108,26 @@ function Financial() {
               ></Form.Control>
             </Form.Group>
           </Col>
+
           <Col xs={2} md={2} className="mt-md-4">
-            <Button className="mt-2" type="submit">הצג מידע</Button>
+            <Button className="mt-2" type="submit">
+              הצג מידע
+            </Button>
           </Col>
+
+          {income && spending && (
+            <Col xs={12} md={2} className="mt-md-4">
+              <CSVLink
+                data={financialData}
+                onClick={combineDataData}
+                filename={"מידע פיננסי.csv"}
+                asyncOnClick={true}
+                className="btn btn-outline-primary mt-2"
+              >
+                <i className="bi bi-file-earmark-spreadsheet"> </i>הורדת המידע
+              </CSVLink>
+            </Col>
+          )}
         </Row>
       </Form>
 
