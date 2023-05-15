3c9536816423ac410996c498799b03601a4c42c2
Samir Khoury <samir@khoury.tk>
2023-05-15 22:11:21 +0300

    main app page

--- frontend/landing/src/App.jsx
+++ frontend/landing/src/App.jsx
@@ -1,40 +1,24 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 
-//components
+//Components
 import Navbar from "./components/Navbar";
-//Sections
-import Hero from "./components/Sections/Hero";
-import SellingPoints from "./components/Sections/SellingPoints";
-import Features from "./components/Sections/Features";
-import About from "./components/Sections/About";
-import Footer from "./components/Footer";
+//Pages
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         {/* Home page */}
-        <Route
-          path="/"
-          element={
-            <>
-              <Navbar />
-              <Hero />
-              <SellingPoints />
-              <Features />
-              <About />
-              <Footer />
-            </>
-          }
-          exact
-        />
+        <Route path="/" element={<Home />} exact />
 
         {/* Terms and conditions page */}
         <Route path="/terms" element={<></>} exact />
 
         {/* 404 Page for non-existing pages */}
-        <Route path="*" element={<></>} exact />
+        <Route path="*" element={<NotFound />} exact />
       </Routes>
     </div>
   );
